303a241fea0884484e1f52e3c0a3e2c3a723d586
Srikanthk-2000 <135803015+Srikanthk-2000@users.noreply.github.com>
2024-08-14 11:14:48 +0530

    lateast update the about ui

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -17,7 +17,7 @@ const AboutUs = () => {
 
             <div className="flex justify-center p-4">
                 <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-                    At 8 Ways, We are driven by the following principles
+                    At Infin Eightways, We are driven by the following Core principles.
                 </h2>
             </div>
 
